27c639f6bc35c461d848feaf152c01268273899f
Your Name <egorm@proftit.com>
2022-02-14 16:54:32 +0200

    start

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoader', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelectorAll('.tabheader__items');
+
+});
