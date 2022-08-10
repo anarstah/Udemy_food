fd2459e8d3cbf0a81ed6d0d3e5fddf513ccfaf2f
Yegor <egorm@proftit.com>
2022-08-10 12:17:36 +0300

    POST added

--- src/js/main.js
+++ src/js/main.js
@@ -208,4 +208,57 @@ window.addEventListener('DOMContentLoaded', () => {
         21,
         '.menu .container'
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Success',
+        failure: 'Fail'
+    }
+
+    forms.forEach(function (item) {
+        postData(item);
+    })
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', '../server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+
+            const formData = new FormData(form);
+
+            const obj = {};
+
+            formData.forEach((key, value) => {
+                obj[key] = value;
+            })
+
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000)
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        })
+    };
 });
